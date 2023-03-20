256e9e908a50dbc95ff363056a268597e826bddb
Vismaad-Singh <vismaadsingh0026@gmail.com>
2023-03-20 21:55:46 +0530

    second commit

--- script.js
+++ script.js
@@ -1,35 +1,104 @@
+
 const buttonColours = ["red", "blue", "green", "yellow"]
 
 var gamePattern = []
 
 var userClickedPattern = [];
 
+var level = 0;
+var started = false;
 
-$(".btn").on("click",function (){
-    const userChosenColour = $(this).attr("id");
+
+$(document).keypress(function () {
+   if(!started){
+    $("#level-title").text("Level : " + level);
+    nextSequence();
+    started = true;
+   }
+  });
+  
+
+
+
+
+
+
+$(".btn").click(function (){
+   
+    var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+   
 });
+
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("Success");
+    }
+    
+    if(userClickedPattern.length === gamePattern.length){
+        setTimeout(function(){
+            nextSequence();
+        },1000);
+    }
+    
+    
+    else{
+        console.log("wrong");
+    }
+    }
+    
+    
+
 
 
 
 function nextSequence(){
+    
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level : " + level);
+
     const randomNumber =  Math.floor(Math.random() * 4)
     var randomChosenColour = buttonColours[randomNumber];
 gamePattern.push(randomChosenColour)
 
-
 $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
 playSound(userChosenColour);
 
 }
+
 
 function playSound(name){
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
-function animatePress(){
-    
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+      }, 100);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
